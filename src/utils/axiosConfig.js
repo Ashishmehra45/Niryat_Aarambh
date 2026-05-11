8fbfd2a53b681562020acf527e79bd645c99c171
Ashish Mehra <ashish6266mehra@gmail.com>
2026-05-11 12:01:58 +0530

    newwwwww

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Dynamic Base URL
 const BASE_URL =
-  import.meta.env.MODE === "development"
+  window.location.hostname === "localhost"
     ? "http://localhost:5000/api"
     : "https://niryat-aarambh-backend.onrender.com/api";
 
-// Axios instance
+// Axios Instance
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
@@ -18,17 +18,18 @@ api.interceptors.response.use(
     return response;
   },
   (error) => {
+
     if (error.response && error.response.status === 401) {
 
       console.warn("Session Expired or No Cookie Found. Redirecting to Login...");
 
-      // Clear local storage
-      localStorage.removeItem('adminData');
-      localStorage.removeItem('sellerData');
-      localStorage.removeItem('buyerData');
+      // Clear Local Storage
+      localStorage.removeItem("adminData");
+      localStorage.removeItem("sellerData");
+      localStorage.removeItem("buyerData");
 
-      // Redirect
-      window.location.href = '/admin/login';
+      // Redirect to Login
+      window.location.href = "/admin/login";
     }
 
     return Promise.reject(error);
