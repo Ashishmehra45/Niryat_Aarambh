305a67c37a2b36e0fe907f1195cab4e6d492ead9
Ashish Mehra <ashish6266mehra@gmail.com>
2026-05-11 12:19:11 +0530

    neww

--- src/components/protectedRoutes.jsx
+++ src/components/protectedRoutes.jsx
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast';
 
 const ProtectedRoute = ({ role, children }) => {
   
-if (role === 'admin') {
+if (role === 'superadmin') {
   const adminData = JSON.parse(localStorage.getItem('adminData'));
   
   
