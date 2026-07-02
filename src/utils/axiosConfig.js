f89277573316316342fb43dc3425bc4c284eab86
Ashish Mehra <ashish6266mehra@gmail.com>
2026-07-02 13:15:02 +0530

    production

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000/api"
-    : "https://niryat-aarambh-backend.onrender.com/api";
+    : "https://www.transparentb2b.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
